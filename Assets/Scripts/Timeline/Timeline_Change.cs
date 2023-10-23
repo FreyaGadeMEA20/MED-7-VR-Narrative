19089a9beb3adf5092807d04f6e3a813dc9f637f
Nichlaur <nlaurs20@student.aau.dk>
2023-10-23 16:07:47 +0200

    Buffer zone and Binary input starting to take shape

--- Assets/Scripts/Timeline/Timeline_Change.cs
+++ Assets/Scripts/Timeline/Timeline_Change.cs
@@ -6,22 +6,25 @@ using UnityEngine.Playables;
 public class Timeline_Change : MonoBehaviour
 {
 
-    [SerializeField] private PlayableDirector[] Directors;
+    public PlayableDirector Timelines;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(TimelineChange());
+        StartCoroutine(Buffer());
+        
+        Timelines = GetComponent<PlayableDirector>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log((int)Time.time);
+        StartCoroutine(Timeline_Switch());
     }
 
-    IEnumerator TimelineChange()
+    IEnumerator Buffer()
     {
         //Print the time of when the function is first called.
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
@@ -29,7 +32,22 @@ public class Timeline_Change : MonoBehaviour
         //yield on a new YieldInstruction that waits for 2 seconds.
         yield return new WaitForSeconds(2);
 
-        //After we have waited 5 seconds print the time again.
+        //After we have waited 2 seconds print the time again.
         Debug.Log("Finished Coroutine at timestamp : " + Time.time +". Now ready for Input!");
+
+    }
+
+    IEnumerator Timeline_Switch()
+    {
+        yield return new WaitForSeconds(2);
+
+        if (Input.GetButton("Fire1") && Time.time < 15)
+        {
+            Debug.Log ("Logged input result A");
+        } 
+        else if (Input.GetButton("Fire1") && Time.time > 15)
+        {
+            Debug.Log ("Logged input result B");
+        }
     }
 }
