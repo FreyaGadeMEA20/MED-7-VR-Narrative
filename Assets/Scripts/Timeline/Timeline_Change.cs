78a6c4e48b3a237402f224879f8295a245f67386
Nichlaur <nlaurs20@student.aau.dk>
2023-11-08 14:12:28 +0100

    Timeline change now works
    Basic timeline change works
    Timeline change based on overall blink rate (OBR) works
    Data is written to auto generated csv file

--- Assets/Scripts/Timeline/Timeline_Change.cs
+++ Assets/Scripts/Timeline/Timeline_Change.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -9,15 +10,37 @@ public class Timeline_Change : MonoBehaviour
     public int switchTime;
     public int buffer;
     private int cd = 0;
-
+    private int pd;
+    
     public List<PlayableDirector> playableDirectors;
     private PlayableDirector playableDirector_Current;
+    private PlayableDirector playableDirector_Previous;
+    
+    private float pressRate;
+    private float averagePressRate;
+    private List<float> pressTimestamps;
+    private List<float> pressRates;
+
+    private StreamWriter csvWriter;
 
     // Start is called before the first frame update
     void Start()
     {
         playableDirector_Current = playableDirectors[cd];
         playableDirector_Current.Play();
+
+        pressTimestamps = new List<float>();
+        pressRates = new List<float>();
+
+        // Initialize the StreamWriter to write to a CSV file
+        string filePath = @"Assets\CSV Data\BlinkrateData.csv"; // Set your desired file path
+        csvWriter = new StreamWriter(filePath, true);
+
+         // Check if the file is empty (indicating the start of a new session) and write headers
+        if (csvWriter.BaseStream.Length == 0)
+        {
+            csvWriter.WriteLine("Time, ButtonPressRate, AveragePressRate"); // Write header
+        }
     }
 
     // Update is called once per frame
@@ -26,20 +49,38 @@ public class Timeline_Change : MonoBehaviour
         //Debug.Log("The scene has been running for " + (int)Time.time + " seconds");
         //Debug.Log("The timeline has been running for " + (int)playableDirector_Current.time + " seconds");
         
-        if(playableDirector_Current.time > buffer && cd != 7 || cd != 8 || cd != 13 || cd != 14 || cd != 19 || cd != 20)
+        DirectorStop();
+
+        if (playableDirector_Current.time > buffer && Input.GetKeyDown(KeyCode.Space))
         {
-            SwitchTimeline();
-        } else if (playableDirector_Current.time > buffer)
+            float currentTimestamp = Time.time;
+            pressTimestamps.Add(currentTimestamp);
+            CalculateButtonPressRate();
+
+            csvWriter.WriteLine($"{currentTimestamp}, {pressRate}, {averagePressRate}");
+            csvWriter.Flush(); // Flush to ensure data is written immediately
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && playableDirector_Current.time > buffer && (cd == 5 || cd == 6 || cd == 11 || cd == 12 || cd == 17 || cd == 18 || cd == 21 || cd == 22))
         {
             SwitchTimelineOBR();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && playableDirector_Current.time > buffer)
+        {
+            SwitchTimeline();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Close the StreamWriter when the game is finished
+            csvWriter.Close();
         }
     }
 
     public void SwitchTimeline()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && playableDirector_Current.time < switchTime)
+        if (playableDirector_Current.time < switchTime)
         {
-            Debug.Log ("Timeline switched to" + cd + " at " + playableDirector_Current.time + " seconds");
             cd += 1;
             if (cd % 2 == 0)
             {
@@ -47,18 +88,93 @@ public class Timeline_Change : MonoBehaviour
             }
             playableDirector_Current = playableDirectors[cd];
             playableDirector_Current.Play();
+            Debug.Log ("Timeline switched to " + cd + " at " + playableDirector_Current.time + " seconds");
         }   
-        else if (Input.GetKeyDown(KeyCode.Space) && playableDirector_Current.time > switchTime)
+        else if (playableDirector_Current.time > switchTime)
         {
-            Debug.Log ("Timeline switched to" + cd + " at " + playableDirector_Current.time + " seconds");
             cd += 2;
+            if (cd % 2 == 1)
+            {
+                cd +=1;
+            }
             playableDirector_Current = playableDirectors[cd];
             playableDirector_Current.Play();
+            Debug.Log ("Timeline switched to " + cd + " at " + playableDirector_Current.time + " seconds");
         }
     }
 
     public void SwitchTimelineOBR()
-    {
+    {      
+        if (pressRate < averagePressRate)
+        {
+            cd += 1;
+            if (cd % 2 == 0)
+            {
+                cd +=1;
+            }
+            playableDirector_Current = playableDirectors[cd];
+            playableDirector_Current.Play();
+            Debug.Log ("Timeline switched to " + cd + " at " + playableDirector_Current.time + " seconds with blink rate");
+        }   
+        else if (pressRate > averagePressRate)
+        {
+            cd += 2;
+            if (cd % 2 == 1)
+            {
+                cd += 1;
+            }
+            playableDirector_Current = playableDirectors[cd];
+            playableDirector_Current.Play();
+            Debug.Log ("Timeline switched to " + cd + " at " + playableDirector_Current.time + " seconds with blink rate");
+        }
+        Debug.Log("OBR Method was called");
+    }
 
+    void CalculateButtonPressRate()
+    {
+        if (pressTimestamps.Count < 2)
+        {
+            // Not enough data to calculate a rate yet.
+            return;
+        }
+
+        float deltaTime = pressTimestamps[pressTimestamps.Count - 1] - pressTimestamps[0];
+        int pressCount = pressTimestamps.Count - 1; // Subtract 1 to exclude the first press as it's not between presses.
+
+        pressRate = pressCount / deltaTime;
+
+        pressRates.Add(pressRate);
+
+        // Calculate the average press rate
+        float totalRate = 0f;
+        foreach (float rate in pressRates)
+        {
+            totalRate += rate;
+        }
+
+        averagePressRate = totalRate / pressRates.Count;
+
+        Debug.Log("Button Press Rate: " + pressRate);
+        Debug.Log("Average Press Rate: " + averagePressRate);   
+    }
+
+    public void DirectorStop()
+    {
+        foreach (var director in playableDirectors)
+        {
+            if (director != playableDirector_Current)
+            {
+                director.Stop();
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        // Close the StreamWriter when the application is exiting
+        if (csvWriter != null)
+        {
+            csvWriter.Close();
+        }
     }
 }
