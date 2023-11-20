fa70020cc9eaae10b7ea9b2d1b2427ed1d394677
Lukas Gade Ravnsborg <lravns20@student.aau.dk>
2023-11-20 14:29:33 +0100

    Dlib finished.

--- Assets/Scripts/PythonScripts/BlinkDetector_DLIB.py
+++ Assets/Scripts/PythonScripts/BlinkDetector_DLIB.py
@@ -11,6 +11,8 @@ from scipy.spatial import distance as dist
 # manually too 
 from imutils import face_utils 
 
+import argparse
+
 class BlinkDetector():
     def __init__(self, landmark_predict, cam):
         """
@@ -22,12 +24,19 @@ class BlinkDetector():
         # Camera
         self.cap = cv2.VideoCapture(int(cam)) 
         self.ret, self.frame = self.cap.read() 
-        print(self.frame)
-
         # Variables 
-        self.blink_thresh = 0.45
-        self.succ_frame = 2
-        self.count_frame = 0
+        #self.blink_thresh = 0.25
+        #self.succ_frame = 2
+        #self.count_frame = 0
+
+        # define two constants, one for the eye aspect ratio to indicate
+        # blink and then a second constant for the number of consecutive
+        # frames the eye must be below the threshold
+        self.EYE_AR_THRESH = 0.53
+        self.EYE_AR_CONSEC_FRAMES = 2
+        # initialize the frame counters and the total number of blinks
+        self.COUNTER = 0
+        self.TOTAL = 0
 
         # Eye landmarks 
         (self.L_start, self.L_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"] 
@@ -35,7 +44,15 @@ class BlinkDetector():
 
         # Initializing the Models for Landmark and 
         # face Detection 
+        print("[INFO] loading facial landmark predictor...")
         self.detector = dlib.get_frontal_face_detector() 
+
+        #ap = argparse.ArgumentParser()
+        #print("{}".format(landmark_predict))
+        #ap.add_argument("-p","--shape-predictor", required=True, help="{}".format(landmark_predict))
+        #ap.add_argument("-v","--video",type=str,default="",help=self.cap)
+
+        #args = vars(ap.parse_args())
         self.landmark_predict = dlib.shape_predictor(landmark_predict) 
 
         # Count check
@@ -57,7 +74,7 @@ class BlinkDetector():
         x1 = dist.euclidean(eye[0], eye[3]) 
     
         # calculate the EAR 
-        EAR = (y1+y2) / x1 
+        EAR = (y1+y2) / (x1) 
     
         return EAR
     
@@ -70,7 +87,7 @@ class BlinkDetector():
         img_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY) 
             
         # detecting the faces---# 
-        faces = self.detector(img_gray) 
+        faces = self.detector(img_gray, 0) 
         #print(faces)
         for face in faces: 
             # Establish the dimensions of the face
@@ -96,9 +113,37 @@ class BlinkDetector():
             left_EAR = self.calculate_EAR(lefteye) 
             right_EAR = self.calculate_EAR(righteye) 
 
+            # compute the convex hull for the left and right eye, then
+            # visualize each of the eyes
+            leftEyeHull = cv2.convexHull(lefteye)
+            rightEyeHull = cv2.convexHull(righteye)
+            cv2.drawContours(self.frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(self.frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
             # Avg of left and right eye EAR 
             avg = (left_EAR+right_EAR)/2
-            if avg < self.blink_thresh: 
+
+            # check to see if the eye aspect ratio is below the blink
+            # threshold, and if so, increment the blink frame counter
+            if avg < self.EYE_AR_THRESH:
+                self.COUNTER += 1
+            # otherwise, the eye aspect ratio is not below the blink
+            # threshold
+            else:
+                # if the eyes were closed for a sufficient number of
+                # then increment the total number of blinks
+                if self.COUNTER >= self.EYE_AR_CONSEC_FRAMES:
+                    self.TOTAL += 1
+                # reset the eye frame counter
+                self.COUNTER = 0
+
+            # draw the total number of blinks on the frame along with
+            # the computed eye aspect ratio for the frame
+            cv2.putText(self.frame, "Blinks: {}".format(self.TOTAL), (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(self.frame, "EAR: {:.2f}".format(avg), (300, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            """if avg < self.blink_thresh: 
                 self.count_frame += 1  # incrementing the frame count 
             else: 
                 if self.count_frame >= self.succ_frame: 
@@ -108,7 +153,7 @@ class BlinkDetector():
                     self.counter += 1
                     return 1
                 else: 
-                    count_frame = 0
+                    self.count_frame = 0"""
 
         cv2.imshow("Video", self.frame) 
         if cv2.waitKey(5) & 0xFF == ord('q'): 
